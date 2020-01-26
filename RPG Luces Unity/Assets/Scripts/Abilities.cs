9bcdeecf643a89e77fa0c6e5f4d22af68b7b894a
Juan Pablo Maggi <juanpablomaggiv@gmail.com>
2020-01-25 22:44:54 -0300

    Creo script base de habilidades

---
+++ RPG Luces Unity/Assets/Scripts/Abilities.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour
+{
+// Collection of different habilities
+
+/// <summary>
+/// Makes an objective faster
+/// </summary>
+    public void UniqueHaste ()
+    {
+
+    }
+
+ /// <summary>
+ /// Make all objectives faster
+ /// </summary>
+    public void MultiHaste()
+    {
+        
+    }
+}
