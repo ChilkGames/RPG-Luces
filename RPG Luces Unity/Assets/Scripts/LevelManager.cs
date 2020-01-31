1b93dfe0dcb96c5d4bfd4f0f9b1318c92dc25379
Juan Pablo <juanpablomaggiv@gmail.com>
2020-01-31 02:32:07 -0300

    Cambio proyecto
    Cambio el proyecto base de 2D a 3D

---
+++ RPG Luces Unity/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Loads a scene by it's name
+    /// </summary>
+    /// <param name="sceneName">Scene Name</param>
+    public void LoadSceneByName(string sceneName)
+    {
+       
+    }
+
+    /// <summary>
+    /// Loads a scene by it's index 
+    /// </summary>
+    /// <param name="sceneIndex">Scene Index</param>
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+    
+    }
+}
---
+++ RPG Luces Unity/Assets/Scripts/UnitStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public float health;
+    public float mana;
+    public float attack;
+    public float defense;
+    public float magic;
+    public float speed;
+    public string type;
+}
