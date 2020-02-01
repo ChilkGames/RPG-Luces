edfc02b4541c45465568eb1fd94cae7069abbb4b
Juan Pablo <juanpablomaggiv@gmail.com>
2020-02-01 20:43:47 -0300

    Combat System 0.1
    Diferentes States machines para controlar los estados durante las peleas de los personajes y los enemigos. Tambien un state machine propio para el manager del sistema de combate

---
+++ RPG Luces Unity/Assets/Scripts/PlayerStateMachine.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    public UnitStats stats;
+
+    private BattleStateMachine BSM;
+
+    public enum TurnState
+    {
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+
+    public TurnState actualState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BSM = FindObjectOfType<BattleStateMachine>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (actualState)
+        {
+            case TurnState.WAITING:
+                break;
+            case TurnState.SELECTING:
+                break;
+            case TurnState.ACTION:
+                break;
+            case TurnState.DEAD:
+                break;
+        }
+    }
+}
---
+++ RPG Luces Unity/Assets/Scripts/TurnHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class TurnHandler
+{
+    public string attacker; //Name of the attacker
+    public GameObject attackerGameObject;
+    public List<GameObject> targets; //List of targets of the attack
+}
